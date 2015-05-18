60b53e71a43d6a7f09ff7fedd61f44a3b474d476
Paul Farrow <paul_farrow@live.co.uk>
2015-05-18 01:34:04 +0100

    Added Main class to start application, used dummy hello world just for testing.

---
+++ ava/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Paul
+ * Date: 18/05/2015
+ * Time: 01:30
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
